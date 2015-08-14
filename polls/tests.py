f21db2f1690a55d0416b3ba670d762683025364e
Alex Hurst <alexmhurst@gmail.com>
2015-08-14 03:04:55 -0400

    A whole bunch of unit tests for the index view.

--- polls/tests.py
+++ polls/tests.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 import datetime
 from django.utils import timezone
 from .models import Question
+from django.core.urlresolvers import reverse
 
 class QuestionMethodTests(TestCase):
 	
@@ -33,3 +34,77 @@ class QuestionMethodTests(TestCase):
 		time = timezone.now() - datetime.timedelta(hours=12)
 		recent_question = Question(pub_date = time)
 		self.assertEqual(recent_question.was_published_recently(), True)
+
+def create_question(question_text, days):
+	time = timezone.now() + datetime.timedelta(days=days)
+	return Question.objects.create(question_text=question_text, 
+					pub_date = time)
+
+class QuestionViewTests(TestCase):
+	def test_index_view_with_no_questions(self):
+		"""
+		If no questions exist, an appropriate message should be displayed.
+		"""
+		response = self.client.get(reverse('polls:index'))
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "No polls are available.")
+		self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+	def test_index_view_with_past_question(self):
+		"""
+		If a past question exists, it should be displayed on the index page.
+		"""
+		create_question("Past question.", -1)
+		response = self.client.get(reverse('polls:index'))
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, 'Past question.')
+		self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Past question.>'])
+
+	def test_index_view_with_future_question(self):
+		"""
+		If a future question exists, it should not be displayed on the index page.
+		"""
+		create_question("Future question.", 1)
+		response = self.client.get(reverse('polls:index'))
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, 'No polls are available.', status_code=200)
+		self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+	def test_index_view_with_past_and_future_questions(self):
+		"""
+		If both past and future questions exist, only the past question should be shown on the index page.
+		"""
+		create_question("Past question.", -1)
+		create_question("Future question.", 1)
+		response = self.client.get(reverse('polls:index'))
+		self.assertContains(response, 'Past question.', status_code=200)
+		self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Past question.>'])
+
+
+	def test_index_view_with_two_past_questions(self):
+		"""
+		If two past questions exist, they should both show.
+		"""
+		create_question("Past question 1.", -1)
+		create_question("Past question 2.", -2)
+		response = self.client.get(reverse('polls:index'))
+		self.assertContains(response, 'Past question 1.', status_code=200)
+		self.assertContains(response, 'Past question 2.', status_code=200)
+		self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Past question 1.>', '<Question: Past question 2.>'])
+		
+
+
+	def test_index_view_with_more_than_five_past_questions(self):
+		"""
+		If two past questions exist, they should both show.
+		"""
+		create_question("Past question 1.", -1)
+		create_question("Past question 2.", -2)
+		create_question("Past question 3.", -3)
+		create_question("Past question 4.", -4)
+		create_question("Past question 5.", -5)
+		create_question("Past question 6.", -6)
+		response = self.client.get(reverse('polls:index'))
+		self.assertEqual(len(Question.objects.all()), 6)
+		self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Past question 1.>', '<Question: Past question 2.>',  '<Question: Past question 3.>',  '<Question: Past question 4.>',  '<Question: Past question 5.>'])
+		
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.template import RequestContext, loader
 from django.views import generic
+from django.utils import timezone
 
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Choice, Question
@@ -28,7 +29,8 @@ class IndexView(generic.ListView):
 	
 	def get_queryset(self):
 		"""Return the last five published questions."""
-		return Question.objects.order_by('-pub_date')[:5]
+		return Question.objects.filter(pub_date__lte=timezone.now()
+			).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
 	model = Question
